c62c88270feca6162279f3be40f11801cb27b279
Rodrigo souza <124813706+rodrigosouza974@users.noreply.github.com>
2024-08-17 18:47:09 -0300

    17-08-24

---
+++ ExemploFundamentos.Common/Models/Curso.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExemploFundamentos.Common.Models
+{
+    public class Curso
+    {
+        public string Nome { get; set; }    
+        public List<Pessoa> Alunos { get; set; }
+        public void AdicionarAluno(Pessoa aluno) //metodo adicionar aluno
+        {
+            Alunos.Add(aluno);
+        }
+
+        public int ObterQuantidadeDeAlunosMatriculados()
+        {
+            int quantidade = Alunos.Count;
+            return quantidade;
+        }
+        
+    }
+}
